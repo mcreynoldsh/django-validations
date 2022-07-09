2157cd4a7e952b7c511867c43023fcd69d22c2b0
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-07-09 12:59:44 -0500

    initial

--- swimrecords/models.py
+++ swimrecords/models.py
@@ -1,12 +1,21 @@
 from django.db import models
+from django.core.exceptions import ValidationError # import ValidationError from Django
+from django.utils.translation import gettext_lazy as text
+from django.core.validators import * 
+from django.utils import timezone
+from datetime  import datetime
+
+def validate_stroke_type(stroke_type):
+    valid_stroke_types = ['front crawl', 'butterfly', 'breast', 'back', 'freestyle']
+    if stroke_type not in valid_stroke_types:
+        raise ValidationError(text(f"{stroke_type} is not valid stroke"))
 
 class SwimRecord(models.Model):
-    pass # delete me when you start writing in validations
-    # first_name = models.CharField()
-    # last_name = models.CharField()
-    # team_name = models.CharField()
-    # relay = models.BooleanField()
-    # stroke = models.CharField()
-    # distance = models.IntegerField()
-    # record_date = models.DateTimeField()
-    # record_broken_date = models.DateTimeField()
+    first_name = models.CharField(max_length=61)
+    last_name = models.CharField(max_length=61)
+    team_name = models.CharField(max_length=61)
+    relay = models.BooleanField()
+    stroke = models.CharField(max_length=61, validators=[validate_stroke_type])
+    distance = models.IntegerField(validators=[MinValueValidator(50)])
+    record_date = models.DateTimeField(validators=[MaxValueValidator(timezone.now())])
+    record_broken_date = models.DateTimeField(validators=[MinValueValidator(record_date)])
